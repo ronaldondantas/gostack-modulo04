9da5e9418fc7b0a30017385428e9fa00204ef9b2
Ronaldo Dantas <ronaldo.dantas@polibrasnet.com.br>
2019-10-09 00:02:53 -0300

    first commit

---
+++ src/components/TechList.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import TechItem from "./TechItem";
+
+// import { Container } from './styles';
+
+export default class TechList extends Component {
+  state = {
+    newTech: "",
+    techs: ["Node.js", "ReactJS", "ReactNative"]
+  };
+
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  componentWillUnmount() {}
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({
+      techs: this.state.techs.filter(techItem => techItem !== tech)
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        ></input>
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
